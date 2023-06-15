2191979e439e76f87660f43e92204bb005ecd37c
Manan Kataria <katariamanan1997@gmail.com>
2023-06-15 18:17:56 +0530

    Contact form added

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -7,26 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
- 
+  constructor(
+    private fb: FormBuilder
+  ) { }
+  contactForm: any;
   contactInfo = [
     {
-      "property" : "Name",
-      "value" : "Manan Kataria",
-      "icon" : "bi bi-person-fill"
+      "property": "Name",
+      "value": "Manan Kataria",
+      "icon": "bi bi-person-fill"
     },
     {
-      "property" : "Address",
-      "value" : "Delhi, India",
-      "icon" : "bi bi-pin-map-fill"
+      "property": "Address",
+      "value": "Delhi, India",
+      "icon": "bi bi-pin-map-fill"
     },
     {
-      "property" : "Email",
-      "value" : "katariamanan1997@gmail.com",
-      "icon" : "bi bi-envelope-fill"
+      "property": "Email",
+      "value": "katariamanan1997@gmail.com",
+      "icon": "bi bi-envelope-fill"
     }
   ]
   ngOnInit(): void {
+    this.contactForm=this.fb.group({
+      "name" : ["", [Validators.required]],
+      "email" : ["", [Validators.required, Validators.email]],
+      "subject" : ["", [Validators.required]],
+      "body" : ["", [Validators.required]]
+    })
+  }
+
+  onSubmit(){
+    console.log("Form submitted", this.contactForm);
   }
 
 }
